19f8dc1048d6f4a2843ece0cb853f5f55a1f6c9c
David Nguyen <phuc.dav@gmail.com>
2021-11-29 12:17:43 +0100

    routing module and material module
    Material module is added and injected to app module for readability.
    Routing module is without any routs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularMaterialModule} from "./angular-material.module";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -11,6 +13,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AngularMaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
