6faafff78f26cc36b051f0e6018fec13ec76b908
David Nguyen <phuc.dav@gmail.com>
2021-11-29 17:19:30 +0100

    Basic sign up
    without email requirements
    not all errors are handled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,33 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import {AngularMaterialModule} from "./angular-material.module";
 import {AppRoutingModule} from "./app-routing.module";
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule, HttpClientXsrfModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'hfian-vjarm-mg3216a4b6da13ryra451b3czf1d6a8f4d68g4a8-f1a6f1ety3a4f3d54gs6y1a3d2c1s6a5',
+      headerName: 'HTTP_X_XSRF_TOKEN',
+    }),
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
