9c9e77437c0e95eeb738e8979f302bdaf5ee6381
David Nguyen <phuc.dav@gmail.com>
2021-11-30 17:54:37 +0100

    Added XSRF module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularMaterialModule} from "./angular-material.module";
 import {AppRoutingModule} from "./app-routing.module";
+import {HttpClientXsrfModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,11 @@ import {AppRoutingModule} from "./app-routing.module";
     BrowserModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'hfian-vjarm-mg3216a4b6da13ryra451b3czf1d6a8f4d68g4a8-f1a6f1ety3a4f3d54gs6y1a3d2c1s6a5',
+      headerName: 'HTTP_X_XSRF_TOKEN',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
