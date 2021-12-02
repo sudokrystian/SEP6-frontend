ffdf15bf3b23224f37120cdbf2d01bc116b961d5
sudokrystian <krystian963@gmail.com>
2021-12-02 13:40:36 +0100

    Working slider

--- src/app/components/trending-movies/trending-movies.component.ts
+++ src/app/components/trending-movies/trending-movies.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { TrendingMovies } from 'src/app/models/trending-movies.model';
 import { MoviesService } from 'src/app/services/movies/movies.service';
+import 'keen-slider/keen-slider.min.css'
+import KeenSlider from 'keen-slider'
 
 @Component({
   selector: 'app-trending-movies',
@@ -8,15 +10,41 @@ import { MoviesService } from 'src/app/services/movies/movies.service';
   styleUrls: ['./trending-movies.component.scss']
 })
 export class TrendingMoviesComponent implements OnInit {
+  @ViewChildren("sliderRef") sliderRef: QueryList<ElementRef<HTMLElement>> | undefined;
+
+  slider: any = null;
 
   trendingMovies: TrendingMovies | undefined;
   // Poster size in pixels
   posterSize: number = 300;
 
+  numbers = [1, 2, 3, 4, 5]
+
   constructor(private api: MoviesService) { }
 
   ngOnInit(): void {
     this.loadMovies()
+  }
+
+  ngAfterViewInit() {
+    console.log("Sldier init!")
+    this.sliderRef?.changes.subscribe((components: QueryList<ElementRef<HTMLElement>>) => {
+      console.log("Activating slider!")
+      this.slider = new KeenSlider<{}>(components.first.nativeElement, {
+        loop: true,
+        rtl: true,
+        slides: {
+          perView: 3,
+          spacing: 10,
+        },
+      })
+    }
+    );
+
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
   }
 
   loadMovies(): void {
